ccbb6a4896684c05604bbb97bf9965822b325ebe
Diego Longhi <diego@ibservi.com.br>
2023-07-27 10:42:11 -0300

    change startup for use swagger in production

--- src/Diego.MyBooks.WebApi/Program.cs
+++ src/Diego.MyBooks.WebApi/Program.cs
@@ -12,9 +12,11 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 //app.UseHttpsRedirection();
 
